0de82fb3d5f6398acba537353cc2d77987d6d6ae
selt0 <chewedgum12@gmail.com>
2020-02-25 11:24:51 -0600

    Completed and linked JS.

--- rock.js
+++ rock.js
@@ -1,61 +1,137 @@
-let userScore = 0;
+let playerScore = 0;
 let computerScore = 0;
-function game() {
-  let playerChoice = playerSelection();
-  let computerChoice = computerPlay();
+const playerScore_span = document.getElementById('player-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector('.scoreboard');
+const result_p = document.querySelector('.results > p')
+const rock_div = document.getElementById('rock');
+const paper_div = document.getElementById('paper');
+const scissors_div = document.getElementById('scissors');
+const action_p = document.getElementById('action-message')
+const reset_btn = document.querySelector('button')
+const choices_div = document.getElementById('choices');
 
-  function playerSelection() {
-    let userChoice = prompt("What is your move: 'rock', 'paper', or 'scissors'");
-    return userChoice.toLowerCase();
-  }
+
+rock_div.addEventListener('click', function() {
+  game('rock')
+})
+
+paper_div.addEventListener('click', function() {
+  game('paper')
+})
+
+scissors_div.addEventListener('click', function() {
+  game('scissors')
+})
+
+
+//computer selects rock, paper, scissors
+function computerPlay() {
+  //pick random number 0 - 2
+  let selection = Math.floor(Math.random() * 3);
+  let choices = ['Rock', 'Paper', 'Scissors'];
   
-  function computerPlay() {
-    //pick random number 0 - 2
-    let selection = Math.floor(Math.random() * 3);
+  return choices[selection];
+}
 
-    let choices = ['Rock', 'Paper', 'Scissors'];
-    
-    return choices[selection];
+//capitalize first letter of player choice
+const capitalize = (string) => { return string.charAt(0).toUpperCase() + string.slice(1) }
+
+function game(playerChoice) {
+  const playerChoice_div = document.getElementById(playerChoice);
+  const computerChoice = computerPlay();
+
+  switch (playerChoice + computerChoice) {
+    case 'rockScissors':
+    case 'paperRock':
+    case 'scissorsPaper':
+      // playerScore++;
+      win();
+      break;
+    case 'scissorsRock':
+    case 'rockPaper':
+    case 'paperScissors':
+      // computerScore++;
+      lose();
+      break;
+    case 'rockRock':
+    case 'paperPaper':
+    case 'scissorsScissors':
+      draw();
   }
 
-  function playRound(playerChoice, computerChoice) {
-    let userWin = `USER WINS! ${playerChoice} beats ${computerChoice}.`;
+    // player wins
+  function win() {
+    playerScore++;
+    playerScore_span.innerHTML = playerScore; 
+    result_p.innerHTML = `PLAYER WINS! ${capitalize(playerChoice)} beats ${computerChoice}.`;
+    playerChoice_div.classList.add('bg-success');
+    setTimeout(function() {
+      playerChoice_div.classList.remove('bg-success')
+    }, 300);
+  }
 
-    let computerWin = `COMPUTER WINS! ${computerChoice} beats ${playerChoice}.`;
+  //player loses
+  function lose() {
+    computerScore++
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `COMPUTER WINS! ${computerChoice} beats ${capitalize(playerChoice)}.`;
+    playerChoice_div.classList.add('bg-danger');
+    setTimeout(function() {
+      playerChoice_div.classList.remove('bg-danger')
+    }, 300);
+  }
 
-    switch (playerChoice.toLowerCase() + computerChoice) {
-      case 'rockScissors':
-      case 'paperRock':
-      case 'scissorsPaper':
-        userScore++;
-        return userWin;
-        break;
-      case 'scissorsRock':
-      case 'rockPaper':
-      case 'paperScissors':
-        computerScore++;
-        return computerWin;
-        break;
-      case 'rockRock':
-      case 'paperPaper':
-      case 'scissorsScissors':
-        return 'DRAW!';
+  //draw
+  function draw() {
+    result_p.innerHTML = 'DRAW!'
+    playerChoice_div.classList.add('bg-secondary');
+    setTimeout(function() {
+      playerChoice_div.classList.remove('bg-secondary')
+    }, 300);
+  }
+
+  if (playerScore == 5 || computerScore == 5) {
+    endGame();
+  }
+
+}
+
+  //function to disable game and restart
+  function endGame() {
+    if (playerScore == 5) {
+      action_p.innerHTML = 'Game Over. You Won! Play again?';
+      action_p.classList.add('bg-success');
+    } else {
+      action_p.innerHTML = 'Game Over. You Lost! Play again?';
+      action_p.classList.add('bg-danger');
     }
+
+    //disable choices
+    choices_div.classList.add('d-none');
+
+    //display button reset
+    reset_btn.classList.remove('d-none');
   }
 
-  for (let i = 0;i < 5; i++) {
-    playerSelection();
-    computerPlay();
-    console.log(playRound(playerChoice, computerChoice));
-  }
-  
-}
+  reset_btn.addEventListener('click', function() {
+    //reset scores
+    playerScore = 0;
+    playerScore_span.innerHTML = playerScore;
+    computerScore = 0;
+    computerScore_span.innerHTML = computerScore;
 
-game();
-if (userScore > computerScore) {
-    console.log(`${userScore} to ${computerScore}. User Wins!`)
-  } else if (userScore < computerScore) {
-    console.log(`${userScore} to ${computerScore}. Computer Wins!`)
-  } else {
-    console.log(`${userScore} to ${computerScore}. It's a draw!`);
-}
+    //bring back choices
+    choices_div.classList.remove('d-none');
+
+    //hide button
+    reset_btn.classList.add('d-none');
+
+    //reset display message
+    action_p.innerHTML = 'Make your move!';
+    action_p.classList.remove('bg-success');
+    action_p.classList.remove('bg-danger');
+
+    //reset result message
+    result_p.innerHTML = 'Are you feeling lucky? First to 5 wins!'
+  })
